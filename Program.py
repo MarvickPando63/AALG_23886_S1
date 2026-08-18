9144c2b81d98cc4ee2be5ad7a49bf917f5f92982
Marvick Pando <mpandotirado@gmail.com>
2026-08-17 21:09:03 -0500

    Código de Python básico

---
+++ Program.py
@@ -0,0 +1,14 @@
+#C#
+# Console.WriteLine("Cual es tu nombre");
+# string nom = Console.ReadLine();
+# Console.Writeline("Cual es tu edad: ");
+# int edad = Convert.ToInt32(Console.ReadLine());
+# Console.WriteLine($"Te llamas {nom} y tienes {edad} años");
+
+#Python
+
+print("Cual es tu nombre:")
+nom : str = input()
+print("Cual es tu edad:")
+edad : int = int(input())
+print(f"Te llamas {nom} y tienes {edad} años")
